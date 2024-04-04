6b647e3d6c1aad8b8752546b36fe3b9478430d7f
neerajnerlekar <neeraj.nerlekar@gmail.com>
2024-04-03 21:40:23 -0600

    add smart account login card

---
+++ packages/nextjs/components/SmartaccountConnector.tsx
@@ -0,0 +1,12 @@
+// import React from 'react'
+
+const SmartaccountConnector = () => {
+  return (
+    <div>
+      <h1 className="text-lg pb-4">Login To Your Smart Account</h1>
+      <button className="btn primary">Connect</button>
+    </div>
+  );
+};
+
+export default SmartaccountConnector;
